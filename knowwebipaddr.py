069238c1808c50b28855fa2fdaf538eb83b4dd76
globalvar <dwordnavneet@gmail.com>
2015-09-07 22:43:34 +0530

    Update knowwebipaddr.py
    #use this script as 
    python knowwebipaddr --name localhost
    #it will output
    127.0.0.1
    #try it with other webaddresses.

--- knowwebipaddr.py
+++ knowwebipaddr.py
@@ -1,1 +1,15 @@
+__author__ = 'root'
 
+import argparse
+import socket
+
+x = argparse.ArgumentParser(description="Here should be some kind of description")
+x.add_argument('--name',type=str,help="This should be the website name e.g example.com",required=True)
+x.add_argument('-o',type=str,help="This should show/print the ip addr of the entered website",required=False)
+
+#var y as cmdargs
+y = x.parse_args()
+i_var= y.name
+
+ipaddr = socket.gethostbyname(i_var);
+print(ipaddr);
